a41b730ea5ce364df48b58f804872b7fc42441f3
YulianSan <knoob0733@gmail.com>
2023-06-14 16:41:06 -0300

    Criando um componente cell e deixando o estilo um pouco responsivo

---
+++ app/View/Components/Tables/CellProduct.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\View\Components\Tables;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class CellProduct extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public string $value
+    ){ }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.tables.cell-product');
+    }
+}
